775edfe8260c18279791f1594c6a3876ca6d4d7f
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-01-24 11:12:57 +0100

    Develop (#5)
    * Worked on getting the most busy months statistics using pipeline aggregation
    
    * created a durationWeeks virtual property on the tour model
    
    * use a mongoose document pre middleware to add a slug for a tour
    
    * Finished working on the document middleware
    
    * Worked on aggragates and query middles wares
    
    Got how long a query took from when it started and excluded secret tours from all statistical data or from being sent in with the normal tours
    
    * Added validation to the tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,12 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour name must be provided'],
       unique: true,
       trim: true,
+      maxLength: [
+        40,
+        'A tour name must have less than or equal to 40 Characters',
+      ],
+      minLength: [10, 'A tour name must have more than 10 Characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contain alphabets'],
     },
     slug: String,
     duration: {
@@ -21,10 +28,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour difficulty must be provided'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -34,7 +47,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour price must be provided'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // the "this" keyword points to the current document when creating a new one and wouldn't work when updating
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
@@ -70,7 +92,8 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
-// DOCUMENT MIDDLEWARE: Runs before the .save() and .create() and not on .insertMany()
+// DOCUMENT MIDDLEWARE: Runs before the .save() and .create() and not on .insertMany() or update
+
 
 //We call this a Pre save hook or middleware
 tourSchema.pre('save', function (next) {
