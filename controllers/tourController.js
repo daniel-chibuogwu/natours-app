ce2abe0eb69131b2d9bda2f2f03e525ede042f39
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-01-30 21:47:26 +0100

    Added a global catch error function to the tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -53,23 +53,18 @@ exports.getTour = async (req, res) => {
     });
   }
 };
-
-exports.createTour = async (req, res) => {
-  try {
-    const newTour = await Tour.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
+const catchAsync = (fn) => {
+  fn(req, res, next).catch((err) => next(err));
 };
+exports.createTour = catchAsync(async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
+  });
+});
 
 exports.updateTour = async (req, res) => {
   try {
