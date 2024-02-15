aafbe15e8d2f4c8e762d9d6c7d7b935797ca4ffc
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-02-16 00:22:10 +0100

    Develop (#9)
    * Worked on getting the most busy months statistics using pipeline aggregation
    
    * created a durationWeeks virtual property on the tour model
    
    * use a mongoose document pre middleware to add a slug for a tour
    
    * Finished working on the document middleware
    
    * Worked on aggragates and query middles wares
    
    Got how long a query took from when it started and excluded secret tours from all statistical data or from being sent in with the normal tours
    
    * Added validation to the tour schema
    
    * Started error handling
    
    installed chrome ndb and created a global error handler. Also handled errors for unhandled routes
    
    * Added a global catch error function to the tour controller
    
    * Handling 404 errors and properly refactoring for error handling
    
    * Refining error handling for dev and prod enviromnents to make erros more meaningful
    
    * Update errorController.js
    
    Handling operational errors like cast, validation and duplication errors in our mongo app
    
    * Update server.js
    
    Handlling unhandled promise rejections for asychronous errors with server
    
    * Concluded on Error handling by catching unhandled rejections and uncaught exceptions
    
    * Created my user model, updated the controller
    
    Now I can create users and query users from my DB

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,52 @@
+//Operational Errors -> CastError, ValidatorError, Duplicate Errors 1100
+
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value: "${err.keyValue.name}". Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errorMessages = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errorMessages.join('. ')}.`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // note that all operational error are ones that were created with the app error class otherwise, it's not
+  // Operational, trusted error: send  message  to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error: don't leat error details
+  } else {
+    // 1) Log error
+    console.error('Error 🧨', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
 
 //Operational Errors -> CastError, ValidatorError, Duplicate Errors 1100
 
