e61532f674f550302dd1bb78008c07ef0f1f36aa
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-02-17 20:53:19 +0100

    Created a user, sign in with JWT and used an expiration time of 90d. Made sure that the user is signed in immediately after creation and send the token back as a payload

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,23 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // For security purposes we won't use req.body directly to only allow the data specified to be store in the DB
+  const { name, email, password, passwordConfirm } = req.body;
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
