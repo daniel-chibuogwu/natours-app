08d6845ac6fcb3fb5e14126daef1f4df15e64d9c
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-02-19 13:19:47 +0100

    Develop (#13)
    * Worked on getting the most busy months statistics using pipeline aggregation
    
    * created a durationWeeks virtual property on the tour model
    
    * use a mongoose document pre middleware to add a slug for a tour
    
    * Finished working on the document middleware
    
    * Worked on aggragates and query middles wares
    
    Got how long a query took from when it started and excluded secret tours from all statistical data or from being sent in with the normal tours
    
    * Added validation to the tour schema
    
    * Started error handling
    
    installed chrome ndb and created a global error handler. Also handled errors for unhandled routes
    
    * Added a global catch error function to the tour controller
    
    * Handling 404 errors and properly refactoring for error handling
    
    * Refining error handling for dev and prod enviromnents to make erros more meaningful
    
    * Update errorController.js
    
    Handling operational errors like cast, validation and duplication errors in our mongo app
    
    * Update server.js
    
    Handlling unhandled promise rejections for asychronous errors with server
    
    * Concluded on Error handling by catching unhandled rejections and uncaught exceptions
    
    * Created my user model, updated the controller
    
    Now I can create users and query users from my DB
    
    * Update the create user API by using the authController instead of the usercontroller and calling it signup
    
    * Added a validator for confirming password
    
    * Successfully Hashing passwords using Bcrypt before persisting on DB using a pre middleware on the save event
    
    * Created a user, sign in with JWT and used an expiration time of 90d. Made sure that the user is signed in immediately after creation and send the token back as a payload
    
    * Created a login route to login and verify users
    
    * Saving update and added comment for the correct password function
    
    * Handled JWT expired and invalid errors and also protected routes 🤯
    
    * Worked on authorization and permissions allow by allow only certain roles delete a tour

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -23,6 +23,10 @@ router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .delete(
+    authController.protect, // must run before retrictTo because of user info
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour,
+  );
 
 module.exports = router;
