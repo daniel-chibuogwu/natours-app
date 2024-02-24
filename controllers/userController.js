fb808a686c04f177636fade21ad6bdde602aceb0
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-02-24 21:33:25 +0100

    Develop (#17)
    * Worked on getting the most busy months statistics using pipeline aggregation
    
    * created a durationWeeks virtual property on the tour model
    
    * use a mongoose document pre middleware to add a slug for a tour
    
    * Finished working on the document middleware
    
    * Worked on aggragates and query middles wares
    
    Got how long a query took from when it started and excluded secret tours from all statistical data or from being sent in with the normal tours
    
    * Added validation to the tour schema
    
    * Started error handling
    
    installed chrome ndb and created a global error handler. Also handled errors for unhandled routes
    
    * Added a global catch error function to the tour controller
    
    * Handling 404 errors and properly refactoring for error handling
    
    * Refining error handling for dev and prod enviromnents to make erros more meaningful
    
    * Update errorController.js
    
    Handling operational errors like cast, validation and duplication errors in our mongo app
    
    * Update server.js
    
    Handlling unhandled promise rejections for asychronous errors with server
    
    * Concluded on Error handling by catching unhandled rejections and uncaught exceptions
    
    * Created my user model, updated the controller
    
    Now I can create users and query users from my DB
    
    * Update the create user API by using the authController instead of the usercontroller and calling it signup
    
    * Added a validator for confirming password
    
    * Successfully Hashing passwords using Bcrypt before persisting on DB using a pre middleware on the save event
    
    * Created a user, sign in with JWT and used an expiration time of 90d. Made sure that the user is signed in immediately after creation and send the token back as a payload
    
    * Created a login route to login and verify users
    
    * Saving update and added comment for the correct password function
    
    * Handled JWT expired and invalid errors and also protected routes 🤯
    
    * Worked on authorization and permissions allow by allow only certain roles delete a tour
    
    * Implementing password reset functionality and sending reset password link to email using mailtrap and nodemailer 🤯
    
    * still working on the reset password functionality of the natours app
    
    * Finished implementing the password reset functionality
    
    * Updating a logged in current user password
    
    * Create a new route call /updateMe for updating current user data like name and email

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,14 @@ const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
   res.status(200).json({
@@ -11,6 +19,32 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: {
       users,
     },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // Remember that we get the user from req.user when using a 'Protected Route' by adding it as a middleware at the route definition
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400,
+      ),
+    );
+  }
+  // 2) Update the user document
+  // we need to filter the body so that the user don't update anything (like role: 'admin') else asides the name and email.
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  console.log({ filteredBody });
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: { user: updatedUser },
   });
 });
 
