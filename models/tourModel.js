983b816ff43535c072cc8acb6f20c0bc73800309
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-03-12 16:25:16 +0100

    Implemented Embedding tour guides to tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
 
+const User = require('./userModel');
+
 //Schema defines the structure of documents in a MongoDB collection,
 const tourSchema = new mongoose.Schema(
   {
@@ -107,6 +109,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -123,6 +126,13 @@ tourSchema.virtual('durationWeeks').get(function () {
 //We call this a Pre save hook or middleware
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// To Embeb Guides
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
