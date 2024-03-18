3f5e83487ad31accd5511e226aac933b93a3ef09
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-03-18 13:14:38 +0100

    Working on an aggregate for getting the rating and avg rating. Fun stuff

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -49,6 +49,32 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  // IN a static method, the this keyword points to the MODEL and that's why we are using a static method to have access to the Model so we can use aggregates
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log({ stats });
+};
+
+//using post so that the current can be saved first before calculations
+//This points to the current review document being saved
+// We have to use constructor because we don't have the Review model defined before this and we can't put it after
+//it because of the Review Schema which is use to create the model must also have this middleware
+reviewSchema.post('save', function () {
+  // this is the document and constructor is the model that created the document
+  this.constructor.calcAverageRatings(this.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
