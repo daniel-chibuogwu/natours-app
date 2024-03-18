726596b8b956cdfbd872ff65e0e585c5bfce2dfd
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-03-18 16:42:11 +0100

    Using a static method on the review model to calculate the number of ratings and the average rating on a tour using an aggregation pipeline and finally updating the mentioned tour with this data anytime a new tour is save using the post middleware

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -50,7 +51,7 @@ reviewSchema.pre(/^find/, function (next) {
 });
 
 reviewSchema.statics.calcAverageRatings = async function (tourId) {
-  // IN a static method, the this keyword points to the MODEL and that's why we are using a static method to have access to the Model so we can use aggregates
+  // IN a static method, 'this' keyword points to the MODEL and that's why we are using a static method to have access to the Model so we can use aggregates
   const stats = await this.aggregate([
     {
       $match: { tour: tourId },
@@ -63,15 +64,20 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
       },
     },
   ]);
-  console.log({ stats });
+
+  // Updating the tour with the stats
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
 };
 
-//using post so that the current can be saved first before calculations
-//This points to the current review document being saved
+//using 'post' so that the current can be saved first before calculations
 // We have to use constructor because we don't have the Review model defined before this and we can't put it after
-//it because of the Review Schema which is use to create the model must also have this middleware
+//it because of the Review Schema which is use to create the model must also have this middleware before it's used to create the model
+// 'this' is the document and constructor is the model that created the document
+// NOTE: The post middleware does not get access to next
 reviewSchema.post('save', function () {
-  // this is the document and constructor is the model that created the document
   this.constructor.calcAverageRatings(this.tour);
 });
 
