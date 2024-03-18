33aa8362c86ebed78043c0e07a3bf3df6c67a058
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-03-18 17:28:34 +0100

    Updating tours whenever a review is updated or deleted using the calc ratings static method. This was amazing

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -51,8 +51,9 @@ reviewSchema.pre(/^find/, function (next) {
 });
 
 reviewSchema.statics.calcAverageRatings = async function (tourId) {
-  // IN a static method, 'this' keyword points to the MODEL and that's why we are using a static method to have access to the Model so we can use aggregates
+  // IN a static method, 'this' keyword points to the MODEL not the schema or document and that's why we are using a static method to have access to the Model so we can use aggregates
   const stats = await this.aggregate([
+    // Getting all review that matches with for the Tour by ID
     {
       $match: { tour: tourId },
     },
@@ -65,20 +66,41 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
     },
   ]);
 
-  // Updating the tour with the stats
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].avgRating,
-  });
+  // Whenever there is no match it returns an empty array and no rating and average so the do this check to handle the error
+  if (stats.length > 0) {
+    // Updating the tour with the stats
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
 
-//using 'post' so that the current can be saved first before calculations
+//using 'post' so that calculations can be made after the review has been saved
 // We have to use constructor because we don't have the Review model defined before this and we can't put it after
 //it because of the Review Schema which is use to create the model must also have this middleware before it's used to create the model
 // 'this' is the document and constructor is the model that created the document
 // NOTE: The post middleware does not get access to next
 reviewSchema.post('save', function () {
   this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // In a query middleware we only have access to the query but we are using the query to get the document
+  // doing this to pass a value from the 'pre' to the 'post' middleware
+  this.r = await this.findOne();
+  next();
+});
+
+// We are using POST here to get access to the updated data
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); does NOT work where because the query has already executed and we don't can't use it again to get the document
+  await this.r.constructor.calcAverageRatings(this.r.tour); // r is the review passed from the pre to the post middleware. r is a document and we can use the constructor as the Model we need 😂😂😂
 });
 
 const Review = mongoose.model('Review', reviewSchema);
