6549671aada92f32bb6a682edc0e5df9dbc0247c
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-04-18 15:38:25 +0100

    Pausing on the stripe integration as I need to read more on it and it's not necessary for now

---
+++ controllers/bookingController.js
@@ -0,0 +1,37 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Tour = require('../models/tourModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // 1) Get the currently booked tour
+  const tour = await Tour.findById(req.params.tourID);
+  if (!tour) return new next(new AppError("Can't find tour for booking!", 404));
+
+  // 2) Create checkout session
+  const session = await stripe.checkout.sessions.create({
+    // Information about the session
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email, // remember that this is a protected router and the user is always on the req object because of this,
+    client_reference_id: req.params.tourID,
+    // Information about the product
+    line_items: [
+      {
+        name: `${tour.name} Tour`,
+        description: tour.summary,
+        images: [`https://www.natours.dev/img/tours/${tour.imageCover}.jpg`],
+        price: tour.price * 100,
+        currency: 'usd',
+        quantity: 1,
+      },
+    ],
+  });
+
+  // 3) Create session as response
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
